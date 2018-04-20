f12375b2a189e308d393a0d9e7ab7c61815cc9e9
lucas8 <luc.linux@mailoo.org>
2018-04-20 22:34:12 +0200

    Prove the correction of the binding-substitution transformation

--- logic/default.nix
+++ logic/default.nix
@@ -5,6 +5,6 @@ let
   myTexLive = with pkgs; texlive.combine { inherit (texlive) scheme-full; };
 in pkgs.stdenv.mkDerivation {
   name = "logic-latex";
-  buildInputs = with pkgs; [ less man myTexLive gnumake ] ++ packages;
+  buildInputs = with pkgs; [ less man myTexLive gnumake dot2tex graphviz ] ++ packages;
 }
 
