0e2ee2cf9c36ee4e8c5d87a1efd7445f209384d6
lucas8 <luc.linux@mailoo.org>
2018-06-27 00:52:13 +0200

    Improve error reporting

--- interface/src/main.cpp
+++ interface/src/main.cpp
@@ -2,10 +2,17 @@
 #include <iostream>
 #include "app.hpp"
 
+using namespace std;
 
 int
 main(int argc, char* argv[])
 {
-  App app(argc, argv);
-  return app.exec();
+    try{
+        App app(argc, argv);
+        return app.exec();
+    } catch(const char* str) {
+        cout << "Exception: " << str << endl;
+    } catch(const std::string& str) {
+        cout << "Exception: " << str << endl;
+    }
 }
