6bb3bf758aba55b7bb1c936dc9f16cb13f51d613
lucas8 <luc.linux@mailoo.org>
2018-07-04 02:26:17 +0200

    Use boost::graph::topological_sort to order frames by dependance

--- interface/src/treeSequence.cpp
+++ interface/src/treeSequence.cpp
@@ -1,3 +1,12 @@
 
 #include "treeSequence.hpp"
 
+namespace internal {
+    Edge make_edge(size_t src, size_t trg) {
+        Edge edg;
+        edg.source = src;
+        edg.target = trg;
+        return edg;
+    }
+}
+
--- interface/src/treeSequence.hpp
+++ interface/src/treeSequence.hpp
@@ -11,6 +11,9 @@
 #include <cassert>
 #include <boost/optional.hpp>
 #include <boost/bimap.hpp>
+#include <boost/range/irange.hpp>
+#include <boost/graph/topological_sort.hpp>
+#include <boost/property_map/property_map.hpp>
 
 
 //  ____            _                 _   _             
@@ -154,6 +157,185 @@ size_t TreeSequence<Node,Place>::get_word_from_var(const std::string& var) const
     return m_wordVars.left.find(var)->second;
 }
 
+/* A graph structure for boost
+ * Implements the concepts Graph, VertexListGraph, IncidenceGraph and PropertyGraph
+ * for vertex_index
+ */
+template <typename Node, typename Place>
+struct FrGraph {
+    using Fr = typename FrameGraph<Node,Place>::Frame;
+    FrGraph() = delete;
+    FrGraph(const FrameDico& d) : dico(d) { }
+    std::vector<Fr> vertices;
+    const FrameDico& dico;
+};
+
+namespace internal {
+    struct Edge {
+        size_t source, target;
+        bool operator==(const Edge& edg) {
+            return source == edg.source && target == edg.target;
+        }
+        bool operator!=(const Edge& edg) {
+            return source != edg.source || target != edg.target;
+        }
+    };
+    Edge make_edge(size_t src, size_t trg);
+
+    template <typename Node,typename Place>
+    struct FrGraphEdgeIterator {
+        typename std::vector<typename FrGraph<Node,Place>::Fr>::const_iterator actual;
+        size_t start;
+        const FrGraph<Node,Place>* graph;
+
+        FrGraphEdgeIterator() : actual(), start(0), graph(nullptr)
+                            { }
+
+        bool operator==(const FrGraphEdgeIterator& it) {
+            return (!graph && !it.graph)
+                || (start == it.start && actual == it.actual);
+        }
+
+        bool operator!=(const FrGraphEdgeIterator& it) {
+            return !operator==(it);
+        }
+
+        inline void incr() {
+            if(!graph) return;
+
+            do {
+                ++actual;
+                /* Reached end */
+                if(actual == graph->vertices.end()) {
+                    graph = nullptr;
+                    return;
+                }
+            } while(!graph->dico.related(graph->vertices[start].name, actual->name));
+        }
+
+        inline Edge get() const {
+            return make_edge(start, actual - graph->vertices.begin());
+        }
+
+        Edge operator*() const {
+            return get();
+        }
+
+        FrGraphEdgeIterator& operator++() {
+            incr();
+            return *this;
+        }
+
+        struct DerefPair {
+            Edge pair;
+            Edge operator*() const {
+                return pair;
+            }
+        };
+
+        DerefPair operator++(int) {
+            DerefPair ret;
+            ret.pair = get();
+            incr();
+            return ret;
+        }
+    };
+}
+
+namespace boost {
+
+    class fr_graph_tag : public vertex_list_graph_tag,
+                         public incidence_graph_tag
+    { };
+
+    template <typename Node, typename Place>
+    struct graph_traits<FrGraph<Node,Place>> {
+        /* Graph concept */
+        using vertex_descriptor      = size_t;
+        using edge_descriptor        = ::internal::Edge;
+        using directed_category      = directed_tag;
+        using edge_parallel_category = disallow_parallel_edge_tag;
+        using traversal_category     = fr_graph_tag;
+
+        static vertex_descriptor null_vertex() {
+            return (size_t)-1;
+        }
+
+        /* VertexListGraph concept */
+        using vertex_iterator    =
+            iterator_range<range_detail::integer_iterator<size_t>>::iterator;
+        using vertices_size_type = size_t;
+
+        /* IncidenceGraph concept */
+        using out_edge_iterator = ::internal::FrGraphEdgeIterator<Node,Place>;
+        using degree_size_type  = size_t;
+    };
+}
+
+template <typename Node, typename Place>
+using GTG = boost::graph_traits<FrGraph<Node,Place>>;
+
+template <typename Node, typename Place>
+static std::pair<typename GTG<Node,Place>::vertex_iterator,
+                 typename GTG<Node,Place>::vertex_iterator>
+vertices(const FrGraph<Node,Place>& gr) {
+    auto ir = boost::irange<size_t>(0, gr.vertices.size());
+    return std::make_pair(ir.begin(), ir.end());
+}
+
+template <typename Node, typename Place>
+static typename GTG<Node,Place>::vertices_size_type
+num_vertices(const FrGraph<Node,Place>& gr) {
+    return gr.vertices.size();
+}
+
+template <typename Node, typename Place>
+static typename GTG<Node,Place>::vertex_descriptor
+source(const typename GTG<Node,Place>::edge_descriptor& e,
+       const FrGraph<Node,Place>&) {
+    return e.source;
+}
+
+template <typename Node, typename Place>
+static typename GTG<Node,Place>::vertex_descriptor
+target(const typename GTG<Node,Place>::edge_descriptor& e,
+       const FrGraph<Node,Place>&) {
+    return e.target;
+}
+
+template <typename Node, typename Place>
+static std::pair<typename GTG<Node,Place>::out_edge_iterator,
+                 typename GTG<Node,Place>::out_edge_iterator>
+out_edges(typename GTG<Node,Place>::vertex_descriptor u, 
+          const FrGraph<Node,Place>& gr) {
+    typename GTG<Node,Place>::out_edge_iterator beg, end;
+    beg.graph  = &gr;
+    beg.start  = u;
+    beg.actual = gr.vertices.begin();
+    return std::make_pair(beg, end);
+}
+
+template <typename Node, typename Place>
+static typename GTG<Node,Place>::degree_size_type
+out_degree(typename GTG<Node,Place>::vertex_descriptor u,
+           const FrGraph<Node,Place>& gr) {
+    size_t count = 0;
+    for(auto& fr : gr.vertices) {
+        if(gr.dico.related(gr.vertices[u].name, fr.name)) ++count;
+    }
+    return count;
+}
+
+namespace std {
+    template <typename Node, typename Place>
+    struct iterator_traits<internal::FrGraphEdgeIterator<Node,Place>> {
+        using value_type        = internal::Edge;
+        using difference_type   = long int;
+        using iterator_category = input_iterator_tag;
+        using reference         = internal::Edge&;
+        using pointer           = internal::Edge*;
+    };
+}
 
 template <typename Node, typename Place>
 std::vector<typename TreeSequence<Node,Place>::Frame>
@@ -165,12 +347,20 @@ TreeSequence<Node,Place>::get_word_frames(size_t word_id) const {
         if(m_frames->getFrame(i).lexeme_var == var) ret.push_back(m_frames->getFrame(i));
     }
 
-    /* Works because std::sort supports partial orders */
-    std::sort(ret.begin(), ret.end(),
-            [this](const Fr& f1, const Fr& f2)
-            { return this->m_frames->dico()
-                  .related(f1.name, f2.name); });
+    /* We can't use std::sort because it doesn't support partial orders
+     * but strict weak orderings */
+    // std::sort(ret.begin(), ret.end(),
+    //         [this](const Fr& f1, const Fr& f2)
+    //         { return this->m_frames->dico()
+    //               .related(f1.name, f2.name); });
+    FrGraph<Node,Place> graph(m_frames->dico());
+    graph.vertices = ret;
+    std::vector<size_t> indexes;
+    boost::topological_sort(graph, std::back_inserter(indexes),
+            boost::vertex_index_map(boost::typed_identity_property_map<size_t>()));
 
+    ret.clear();
+    /* TODO refill ret */
     return ret;
 }
 
