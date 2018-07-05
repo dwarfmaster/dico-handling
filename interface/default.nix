33c2c0b22ce0a6e186374e847ac709eab7d95540
lucas8 <luc.linux@mailoo.org>
2018-07-06 00:21:54 +0200

    Add clang as potential compiler

--- interface/default.nix
+++ interface/default.nix
@@ -1,7 +1,7 @@
 { nixpkgs ? import <nixpkgs> {} }:
 let
   inherit (nixpkgs) pkgs;
-  packages = with pkgs; [ qt5.full cmake gnumake gcc pkgconfig boost sourceHighlight ];
+  packages = with pkgs; [ qt5.full cmake gnumake gcc pkgconfig boost sourceHighlight clang ];
 in pkgs.stdenv.mkDerivation {
   name = "nodelearning";
   buildInputs =  packages;
